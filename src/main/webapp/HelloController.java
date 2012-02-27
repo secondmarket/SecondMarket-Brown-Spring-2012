90f832114e082d247c62d5325bdc9911c6f0a960
Justin <justin.ardini@gmail.com>
2012-02-27 18:37:18 -0500

    adding basic controller with unit test

---
+++ src/main/webapp/HelloController.java
@@ -0,0 +1,24 @@
+package webapp;
+
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import java.io.IOException;
+
+public class HelloController implements Controller {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    			
+        logger.info("Returning index view");
+
+        return new ModelAndView("index.jsp");
+    }
+
+}
---
+++ src/test/webapp/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package webapp;
+
+import org.springframework.web.servlet.ModelAndView;
+import webapp.HelloController;
+import junit.framework.TestCase;
+
+public class HelloControllerTest extends TestCase {
+
+    public void testHandleRequestView() throws Exception {		
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("index.jsp", modelAndView.getViewName());
+    }
+}
