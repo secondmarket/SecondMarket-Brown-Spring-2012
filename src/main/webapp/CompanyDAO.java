da294413d5f829a9f4a1fc53da3c01460acfa172
Justin <justin.ardini@gmail.com>
2012-03-19 14:08:27 -0400

    adding searching by industry and location to CompanyDAO

--- src/main/webapp/CompanyDAO.java
+++ src/main/webapp/CompanyDAO.java
@@ -20,7 +20,35 @@ public class CompanyDAO extends BasicDAO<Company, ObjectId> {
 		return ds.find(entityClazz).field("_name").equal(name).asList();
 	}
 	
+	// permalinks are unique, so only return one
 	public Company findByPermalink(String permalink) {
 		return ds.find(entityClazz).field("_permalink").equal(permalink).get();
+	}
+	
+	public List<Company> findByIndustry(String industry) {
+		return ds.find(entityClazz).field("_industry").equal(industry).asList();
+	}
+	
+	// location queries use the first Office by default
+	public List<Company> findByLocation(String city) {
+		return ds.find(entityClazz).disableValidation().field("_offices.0._city").equal(city).asList();		
+	}
+	
+	public List<Company> findByLocation(String city, String state) {
+		return ds.find(entityClazz).disableValidation().field("_offices.0._city").equal(city).
+				 field("_offices.0._state").equal(state).asList();
+	}
+
+	// use these to sort by industry and city, they'll be faster than 
+	// combining findByLocation() and findByIndustry()
+	public List<Company> findByIndustryAndLocation(String industry, String city) {
+		return ds.find(entityClazz).disableValidation().field("_offices.0._city").equal(city).
+				field("_industry").equal(industry).asList();
+	}
+	
+	public List<Company> findByIndustryAndLocation(String industry, String city, String state) {
+		return ds.find(entityClazz).disableValidation().field("_offices.0._city").equal(city).
+				 field("_offices.0._state").equal(state).field("_industry").equal(industry).
+				 asList();		
 	}
 }
